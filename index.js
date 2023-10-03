2c21631bf295eccd059df429d3d8ca6d048cf51a
ndkhanh99 <qhoang.devvietnam@gmail.com>
2023-10-03 17:37:13 +0700

    test+1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 var Client = require('ftp');
 const ftp = require('./FTPClient');
+var ftpClient = require('ftp-client')
  
 const hostname = '127.0.0.1';
 const port = 9000;
@@ -9,8 +10,22 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
 
-    const client = new ftp('ftp.rakuten.ne.jp', 16910, '_partner_53016', '5AXzwF/y', false);
-    client.getList();
+    // const client = new ftp('ftp.rakuten.ne.jp', 16910, '_partner_53016', '5AXzwF/y', false);
+    // client.getList();
+    
+    let config = {
+        host: 'localhost',
+        port: 21,
+        user: 'anonymous',
+        password: 'anonymous@'
+    }
+    let options = {
+        logging: 'basic'
+    }
+    let client = new ftpClient(config, options);
+    client.connect(function () {
+        client.list();
+    });
 
     res.end('ok');
 });
